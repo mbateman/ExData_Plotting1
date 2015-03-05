88b626dab4fb0c55207fbce6f61fc9b94415777a
Michael Bateman <michael.bateman.com@gmail.com>
2015-03-05 21:35:37 +0000

    corrected title for plot1.png

--- plot1.R
+++ plot1.R
@@ -1,8 +1,10 @@
 library(datasets)
 source("load_data.R")
 png(filename="plot1.png", width=480, height=480, units="px")
+par(mfrow = c(1, 1))
 hist(data_s$Global_active_power, 
      col="red", 
      xlab="Global Active Power (kilowatts)", 
-     ylab="Frequency")
+     ylab="Frequency",
+     main = "Global Active Power")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -2,8 +2,10 @@ library(datasets)
 source("load_data.R")
 datetime <- as.POSIXct(data_s$datetime)
 png(filename="plot2.png", width=480, height=480, units="px")
+par(mfrow = c(1, 1))
 plot(datetime, 
      data_s$Global_active_power, 
      type="l", 
-     ylab="Global Active Power (kilowatts)")
+     ylab="Global Active Power (kilowatts)",
+     main=)
 dev.off()
